2834097376fe2e9b8c85e31cc3e5b4f5a25ae1da
Luis Jira <me@luisjira.com>
2024-10-28 19:03:14 +0100

    update network config to link-local addressing and using nmcli for vm setup

--- env_setup.sh
+++ env_setup.sh
@@ -9,21 +9,34 @@ netns()
 
         # Add interfaces to namespaces
         ip link set cx5if1 netns ns_tx
-        ip netns exec ns_tx ip addr add dev cx5if1 10.0.0.1/16
+	ip netns exec ns_tx ip addr add dev cx5if1 169.254.0.1/16
         ip netns exec ns_tx ip link set dev cx5if1 up
+        ip netns exec ns_tx arp -s 169.254.1.4 1c:34:da:54:9a:a4
         ip link set cx5if0 netns ns_rx
-        ip netns exec ns_rx ip addr add dev cx5if0 10.0.1.4/16
+	ip netns exec ns_rx ip addr add dev cx5if0 169.254.1.4/16
         ip netns exec ns_rx ip link set dev cx5if0 up
+        ip netns exec ns_rx arp -s 169.254.0.1 1c:34:da:54:9a:a5
 
         # Execute commands within the namespaces like:
-        # ip netns exec ns_tx iperf -s -B 10.0.0.1
-        # ip netns exec ns_rx iperf -c 10.0.0.1 -B 10.0.0.4
+	# ip netns exec ns_tx iperf -s -B 169.254.0.1
+	# ip netns exec ns_rx iperf -c 169.254.0.1 -B 169.254.0.4
 }
 
 xdp-net()
 {
-        ip addr add 10.0.0.2/24 dev ens16f0np0
-        ip addr add 10.0.1.3/24 dev ens16f1np1
+	# This approach is reset by NetworkManager
+        #ip addr add 10.0.0.2/24 dev ens16f0np0
+        #ip addr add 10.0.1.3/24 dev ens16f1np1
+
+	nmcli conn modify "Wired connection 1" ipv4.addresses "169.254.0.2/24"
+	nmcli conn modify "Wired connection 1" ipv4.method manual
+	nmcli conn down "Wired connection 1"
+	nmcli conn up "Wired connection 1"
+
+	nmcli conn modify "Wired connection 2" ipv4.addresses "169.254.1.3/24"
+	nmcli conn modify "Wired connection 2" ipv4.method manual
+	nmcli conn down "Wired connection 2"
+	nmcli conn up "Wired connection 2"
 }
 
 # Execute argument
