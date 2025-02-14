c8d50186e6eba97b100d0d003acea1eb4d592606
Luis Jira <me@luisjira.com>
2025-02-14 21:12:29 +0100

    add scapy packet generator with progammable packet number and payload size

---
+++ traffic_generator.sh
@@ -0,0 +1,28 @@
+import argparse, netns, time
+from scapy.all import sendp, IP, UDP, Raw, Ether, RandString
+
+def generate_udp_packets(num_packets, payload_size, destination="169.254.1.4", port=12345):
+    """Generates and sends UDP packets to the specified destination."""
+    start_time = time.time()
+
+    if payload_size:
+         s = RandString(size=payload_size)
+         for i in range(num_packets):
+            packet = Ether(dst="1c:34:da:54:9a:a4", src="b8:83:03:6f:63:51") / IP(dst=destination) / UDP(dport=port, sport=port) / Raw(load=s)
+            sendp(packet, iface="cx5if1", verbose=False)
+    else:
+        for i in range(num_packets):
+            packet = Ether(dst="1c:34:da:54:9a:a4", src="b8:83:03:6f:63:51") / IP(dst=destination) / UDP(dport=port, sport=port) / Raw(load="Test Packet "+str(i)+"\n")
+            sendp(packet, iface="cx5if1", verbose=False)
+
+    print(f"Sent {num_packets} UDP packets to {destination}:{port} in {time.time()-start_time}s")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate UDP packets using Scapy.")
+    parser.add_argument("num_packets", type=int, help="Number of packets to send")
+    parser.add_argument("--payload-size", type=int, help="Size of payload to send")
+
+    args = parser.parse_args()
+    print(args)
+
+    generate_udp_packets(args.num_packets,payload_size=args.payload_size)
