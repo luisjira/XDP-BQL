0979d2663786998b469c3ab7571b35dd194f69bf
Luis Jira <me@luisjira.com>
2025-03-04 14:42:57 +0100

    add IPv6 setup

--- env_setup.sh
+++ env_setup.sh
@@ -10,10 +10,12 @@ netns()
         # Add interfaces to namespaces
         ip link set cx5if1 netns ns_tx
 	ip netns exec ns_tx ip addr add dev cx5if1 169.254.0.1/16
+	ip netns exec ns_tx ip addr add dev cx5if1 fe80::1/10
         ip netns exec ns_tx ip link set dev cx5if1 up
         ip netns exec ns_tx arp -s 169.254.1.4 1c:34:da:54:9a:a4
         ip link set cx5if0 netns ns_rx
 	ip netns exec ns_rx ip addr add dev cx5if0 169.254.1.4/16
+	ip netns exec ns_tx ip addr add dev cx5if1 fe80::4/10
         ip netns exec ns_rx ip link set dev cx5if0 up
         ip netns exec ns_rx arp -s 169.254.0.1 1c:34:da:54:9a:a5
 
@@ -31,12 +33,16 @@ xdp-net()
 	# Static IP for ens16f0np0
 	nmcli conn modify "Wired connection 1" ipv4.addresses "169.254.0.2/24"
 	nmcli conn modify "Wired connection 1" ipv4.method manual
+	nmcli conn modify "Wired connection 2" ipv6.addresses "fe80::2/10"
+	nmcli conn modify "Wired connection 2" ipv6.method manual
 	nmcli conn down "Wired connection 1"
 	nmcli conn up "Wired connection 1"
 
 	# Static IP for ens16f1np1
 	nmcli conn modify "Wired connection 2" ipv4.addresses "169.254.1.3/24"
 	nmcli conn modify "Wired connection 2" ipv4.method manual
+	nmcli conn modify "Wired connection 2" ipv6.addresses "fe80::3/10"
+	nmcli conn modify "Wired connection 2" ipv6.method manual
 	nmcli conn down "Wired connection 2"
 	nmcli conn up "Wired connection 2"
 
