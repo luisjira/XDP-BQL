6f239e4ddff29853aa6d94dc401116e20104dd8b
Luis Jira <me@luisjira.com>
2025-03-06 23:43:44 +0100

    add interface choice to pps_recorder

--- packet_per_second_recorder.py
+++ packet_per_second_recorder.py
@@ -1,34 +1,34 @@
+import argparse
 import time
 import csv
 
-INTERFACE = "cx5if0"
-RX_FILE = f"/sys/class/net/{INTERFACE}/statistics/rx_packets"
-OUTPUT_CSV = "rx_packets_per_second.csv"
-
-
-def get_rx_packets():
+def get_rx_packets(interface):
     """Read the number of received packets from the interface statistics."""
+    rx_file = f"/sys/class/net/{interface}/statistics/rx_packets"
     try:
-        with open(RX_FILE, "r") as f:
+        with open(rx_file, "r") as f:
             return int(f.read().strip())
     except Exception as e:
         print(f"Error reading RX packets: {e}")
         return None
 
 
-def monitor_rx_packets(interval=1, duration=60):
+def monitor_rx_packets(interface, out, interval=1, duration=60):
     """Monitor RX packets per second and save to CSV."""
-    with open(OUTPUT_CSV, "w", newline="") as csvfile:
+    if out is None:
+        out = f"pps_{interface}.csv"
+    
+    with open(out, "w", newline="") as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(["Timestamp", "Packets_per_second"])
 
-        prev_packets = get_rx_packets()
+        prev_packets = get_rx_packets(interface)
         if prev_packets is None:
             return
 
         for _ in range(duration):
             time.sleep(interval)
-            current_packets = get_rx_packets()
+            current_packets = get_rx_packets(interface)
             if current_packets is None:
                 continue
 
@@ -40,4 +40,12 @@ def monitor_rx_packets(interval=1, duration=60):
 
 
 if __name__ == "__main__":
-    monitor_rx_packets(interval=1, duration=60)  # Run for 60 seconds
+    parser = argparse.ArgumentParser(description="Show and save packets per second received on interface")
+    parser.add_argument("interface", help="Interface to collect counter from")
+    parser.add_argument("-i","--interval", type=float, default=1, help="Interval to collect data in in seconds")
+    parser.add_argument("-t","--duration", type=int, default=10, help="Time to collect data for in seconds")
+    parser.add_argument("-w","--output-file", type=int, help="Name of output file to write csv data to")
+
+    args = parser.parse_args()
+
+    monitor_rx_packets(args.interface, args.output_file, interval=args.interval, duration=args.duration)
